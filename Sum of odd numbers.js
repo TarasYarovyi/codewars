f128780753ebfb56b08e11b368d1aa246a231019
TarasYarovyi <yarovoy.taras@gmail.com>
2024-01-26 22:42:57 +0100

    Solution

--- Sum of odd numbers.js
+++ Sum of odd numbers.js
@@ -13,11 +13,11 @@
 // 2 --> 3 + 5 = 8
 
 function rowSumOddNumbers(n) {
-  let lastNumber = n ** +(n - 1);
   let result = 0;
+  let lastNumber = n * n + (n - 1);
   for (let i = 0; i < n; i++) {
     result += lastNumber;
-    lastNumber--;
+    lastNumber -= 2;
   }
   return result;
 }
