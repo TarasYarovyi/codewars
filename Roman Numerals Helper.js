10696756a985ed49fb9cff3bc6445aa90391a8c7
TarasYarovyi <yarovoy.taras@gmail.com>
2024-02-10 20:25:48 +0100

    2.1 dictionary solution

--- Roman Numerals Helper.js
+++ Roman Numerals Helper.js
@@ -36,120 +36,37 @@
 // D	500
 // M	1000
 
+const dictionary = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
 class RomanNumerals {
   static toRoman(num) {
-    let result = "";
-    let remainder = num;
-    while (remainder >= 1000) {
-      result += "M";
-      remainder -= 1000;
+    let str = "";
+    for (let i in dictionary) {
+      while (num >= dictionary[i]) {
+        str += i;
+        num -= dictionary[i];
+      }
     }
-    while (remainder >= 900) {
-      result += "CM";
-      remainder -= 900;
-    }
-    while (remainder >= 500) {
-      result += "D";
-      remainder -= 500;
-    }
-    while (remainder >= 400) {
-      result += "CD";
-      remainder -= 400;
-    }
-    while (remainder >= 100) {
-      result += "C";
-      remainder -= 100;
-    }
-    while (remainder >= 90) {
-      result += "XC";
-      remainder -= 90;
-    }
-    while (remainder >= 50) {
-      result += "L";
-      remainder -= 50;
-    }
-    while (remainder >= 40) {
-      result += "XL";
-      remainder -= 40;
-    }
-    while (remainder >= 10) {
-      result += "X";
-      remainder -= 10;
-    }
-    while (remainder === 9) {
-      result += "IX";
-      remainder -= 9;
-    }
-    while (remainder >= 5) {
-      result += "V";
-      remainder -= 5;
-    }
-    while (remainder >= 4) {
-      result += "IV";
-      remainder -= 4;
-    }
-    while (remainder >= 1) {
-      result += "I";
-      remainder -= 1;
-    }
-    return result;
+    return str;
   }
 
   static fromRoman(str) {
-    let result = 0;
-    let remainder = str;
-    while (remainder.substr(0, 1) === "M") {
-      result += 1000;
-      remainder = remainder.slice(1);
-    }
-    while (remainder.substr(0, 2) === "CM") {
-      result += 900;
-      remainder = remainder.slice(2);
-    }
-    while (remainder.substr(0, 1) === "D") {
-      result += 500;
-      remainder = remainder.slice(1);
-    }
-    while (remainder.substr(0, 2) === "CD") {
-      result += 400;
-      remainder = remainder.slice(2);
-    }
-    while (remainder.substr(0, 1) === "C") {
-      result += 100;
-      remainder = remainder.slice(1);
-    }
-    while (remainder.substr(0, 2) === "XC") {
-      result += 90;
-      remainder = remainder.slice(2);
-    }
-    while (remainder.substr(0, 1) === "L") {
-      result += 50;
-      remainder = remainder.slice(1);
-    }
-    while (remainder.substr(0, 2) === "XL") {
-      result += 40;
-      remainder = remainder.slice(2);
-    }
-    while (remainder.substr(0, 1) === "X") {
-      result += 10;
-      remainder = remainder.slice(1);
-    }
-    while (remainder.substr(0, 2) === "IX") {
-      result += 9;
-      remainder = remainder.slice(2);
-    }
-    while (remainder.substr(0, 1) === "V") {
-      result += 5;
-      remainder = remainder.slice(1);
-    }
-    while (remainder.substr(0, 2) === "IV") {
-      result += 4;
-      remainder = remainder.slice(2);
-    }
-    while (remainder.substr(0, 1) === "I") {
-      result += 1;
-      remainder = remainder.slice(1);
-    }
-    return result;
+    return str
+      .match(/CM|CD|XC|XL|IX|IV|\w/g)
+      .reduce((acc, el) => acc + dictionary[el], 0);
   }
 }
