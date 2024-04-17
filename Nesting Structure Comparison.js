e9aae8a1083d06940f705a0c08c8d39fedf906e5
TarasYarovyi <yarovoy.taras@gmail.com>
2024-04-17 20:23:41 +0200

    recursion sum

--- Nesting Structure Comparison.js
+++ Nesting Structure Comparison.js
@@ -1,17 +1,15 @@
 let arr1 = [1, [2, 3]];
 const arr2 = [1, [2, 3]];
+let result = 0;
 
-let result;
 function checkArrays(arrA, arrB) {
   arrA.forEach((elementA) => {
-    arrB.forEach((elementB) => {
-      if (elementA.length) {
-        // checkArrays(elementA, elementB);
-        console.log("ok");
-      } else {
-        console.log("ne ok");
-      }
-    });
+    if (elementA.length) {
+      checkArrays(elementA);
+    } else {
+      result += elementA;
+    }
   });
 }
 checkArrays(arr1, arr2);
+console.log(result);
