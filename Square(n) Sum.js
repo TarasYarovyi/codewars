b9802e216fab0051ab531aa54bdf6e862eebff30
TarasYarovyi <yarovoy.taras@gmail.com>
2024-05-19 21:27:49 +0200

    Currying

--- Square(n) Sum.js
+++ Square(n) Sum.js
@@ -5,3 +5,16 @@
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 // For example, for [1, 2, 2] it should return 9 because  1^1 + 2^2 + 2^2 = 9.
+
+// Currying:
+function squareSum(numbers) {
+  function pow(x) {
+    return function (y) {
+      return Math.pow(y, x);
+    };
+  }
+  const square = pow(2);
+  return numbers.reduce((acc, el) => acc + square(el), 0);
+}
+
+console.log(squareSum([1, 2, 2]));
