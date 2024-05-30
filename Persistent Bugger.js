90ecb3332962e9c07932b75db13d58fa0e449cb3
TarasYarovyi <yarovoy.taras@gmail.com>
2024-05-30 08:16:30 +0200

    Solution

--- Persistent Bugger.js
+++ Persistent Bugger.js
@@ -7,3 +7,13 @@
 // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
 // 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
 // 4 --> 0 (because 4 is already a one-digit number)
+function persistence(num) {
+  let count = 0;
+  let str = String(num);
+
+  while (str.length > 1) {
+    count++;
+    str = Array.from(str).reduce((acc, curr) => acc * curr) + "";
+  }
+  return count;
+}
