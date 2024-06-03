e148b0a38389577f0b1d12ebc5d5c420d281c4ec
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-03 18:27:21 +0200

    Solved

--- Unique In Order.js
+++ Unique In Order.js
@@ -9,4 +9,13 @@
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 
-const uniqueInOrder = function (iterable) {};
+function uniqueInOrder(iterable) {
+  let result = [];
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) {
+      result.push(iterable[i]);
+    }
+  }
+  return result;
+}
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
