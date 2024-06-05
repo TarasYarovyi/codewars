ceef0fc2b0f0308fc63e2d4023dd67838f7fba0e
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-05 18:06:47 +0200

    Solved

--- Enough is enough!.js
+++ Enough is enough!.js
@@ -1,5 +1,6 @@
 // Enough is enough!
 // https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/javascript
+
 // Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection.
 // However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
 // He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number.
@@ -11,5 +12,10 @@
 // With list [20,37,20,21] and number 1, the result would be [20,37,21].
 
 function deleteNth(arr, n) {
-  // ...
+  return arr.reduce((acc, curr) => {
+    if (acc.filter((el) => el === curr).length < n) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
 }
