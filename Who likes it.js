d0912ae2edf6968281bc402a1aa73ede29da2f73
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-06 11:31:13 +0200

    Who likes it?

---
+++ Who likes it.js
@@ -0,0 +1,9 @@
+// Who likes it?
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
