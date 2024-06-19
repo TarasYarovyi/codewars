a7d7e0cdf9456ad98e4720c5378a791dd854cf32
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-19 07:16:12 +0200

    Solved

--- Find the odd int.js
+++ Find the odd int.js
@@ -11,3 +11,10 @@
 // [1,1,2] should return 2, because it occurs 1 time (which is odd).
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+function findOdd(A) {
+  return A.find(
+    (elToFind) =>
+      A.filter((elToFilter) => elToFilter === elToFind).length % 2 > 0
+  );
+}
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
