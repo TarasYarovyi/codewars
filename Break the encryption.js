55326081316703994790ce0fcee54a30628234bd
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-20 21:14:55 +0200

    Break the encryption

---
+++ Break the encryption.js
@@ -0,0 +1,26 @@
+// <!-- Break the encryption
+// https://www.codewars.com/kata/5edb34287115e9002b83e063/train/javascript
+
+// Task
+// You are trying to create a function to decrypt an encrypted message. You have found out that the encryption is weak and simply rearranges the bits in the message.
+
+// Quick Start
+// You are to write a function decrypt(bitsString) taking a string of 0s and 1s representing bits in an encrypted message and returning a string of 0s and 1s with the bits in their original position.
+
+// You have access to an encrypt(bitsString) function that will encrypt a message of your choosing.
+
+// Input
+// A string of 0s and 1s represending an encrypted message
+
+// Output
+// Return a string of 0s and 1s represending the decrypted message
+//  -->
+
+// // return the decrypted bits as a string
+function decrypt(bitsString) {
+  // you have access to an `encrypt(bits)` function that will encrypt
+  // a message of your choosing represented by a string of 0s and 1s.
+  // The encrypt() function will move the bits around in the same order for
+  // every message of the same length and the decrypt() function should
+  // return the bits to their original order
+}
