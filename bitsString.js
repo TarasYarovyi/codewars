df59e2dc666a6da559287c611ddabcfcfa8e46f1
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-21 21:15:16 +0200

    Solution

--- bitsString.js
+++ bitsString.js
@@ -3,5 +3,9 @@
 // https://www.codewars.com/kata/5552101f47fc5178b1000050/train/javascript
 
 function digPow(n, p) {
-  // ...
+  const sum = Array.from(String(n)).reduce((acc, curr) => {
+    return acc + Math.pow(+curr, p++);
+  }, 0);
+  return sum % n === 0 ? sum / n : -1;
 }
+console.log(digPow(46288, 3));
