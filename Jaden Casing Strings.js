25b04c667c9ddecccf49eae4be39b0be496a2081
TarasYarovyi <yarovoy.taras@gmail.com>
2024-06-28 19:48:03 +0200

    Draft

--- Jaden Casing Strings.js
+++ Jaden Casing Strings.js
@@ -9,3 +9,9 @@
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 // Link to Jaden's former Twitter account @officialjaden via archive.org
+String.prototype.toJadenCase = function () {
+  return this.replace(/\b[a-z](?=[a-z]{2})/gi, function (letter) {
+    return letter.toUpperCase();
+  });
+};
+console.log("hor ghj".toJadenCase());
