06f68c900702e433285dd6e964ad54d4e5ba3b12
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-06 08:30:16 +0200

    Solved

--- Array manipulation.js
+++ Array manipulation.js
@@ -1,9 +1,14 @@
 //www.hackerrank.com/challenges/crush/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
 function arrayManipulation(n, queries) {
-  const arr = Array(queries.length).fill(Array(n).fill(0));
+  const arr = Array(n).fill(0);
+  queries.forEach((query) => {
+    for (let i = query[0] - 1; i <= query[1] - 1; i++) {
+      arr[i] += query[2];
+    }
+  });
 
-  return arr;
+  return Math.max(...arr);
 }
 
 console.log(
