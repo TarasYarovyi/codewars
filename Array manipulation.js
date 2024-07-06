027f1675fb3685f71739efa8bfd35a17903b04b3
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-06 08:07:58 +0200

    Arr creating

---
+++ Array manipulation.js
@@ -0,0 +1,15 @@
+//www.hackerrank.com/challenges/crush/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+function arrayManipulation(n, queries) {
+  const arr = Array(queries.length).fill(Array(n).fill(0));
+
+  return arr;
+}
+
+console.log(
+  arrayManipulation(5, [
+    [1, 2, 100],
+    [2, 5, 100],
+    [3, 4, 100],
+  ])
+);
