9327252dbb348a4c8d40125e7afda158e5597891
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-11 17:09:28 +0200

    Solved

--- List Filtering.js
+++ List Filtering.js
@@ -9,5 +9,7 @@
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
 function filter_list(l) {
-  // Return a new array with the strings filtered out
+  return l.filter((el) => typeof el === "number");
 }
+
+console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
