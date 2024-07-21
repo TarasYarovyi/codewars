705e98f9347f76ccc0a6ab9642e46039a4761a53
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-21 17:20:24 +0200

    alternative

--- Categorize New Member.js
+++ Categorize New Member.js
@@ -29,3 +29,10 @@ console.log(
     [104, 20],
   ])
 );
+
+// alternative:
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}
