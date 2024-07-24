bcc8b7e6f1429f38d2059381d78216a7823d1fca
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-24 08:17:09 +0200

    Alternative

--- Build Tower.js
+++ Build Tower.js
@@ -30,4 +30,12 @@ function towerBuilder(nFloors) {
   return tower;
 }
 
+// alternative:
+// function towerBuilder(n) {
+//     return Array.from({length: n}, function(v, k) {
+//       const spaces = ' '.repeat(n - k - 1);
+//       return spaces + '*'.repeat(k + k + 1) + spaces;
+//     });
+//   }
+
 console.log(towerBuilder(6));
